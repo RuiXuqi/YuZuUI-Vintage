cd83b145712d2ff248be0a49aef4215dc6459824
paulzzh <28055560+paulzzh@users.noreply.github.com>
2024-12-14 15:43:56 +0800

    整理代码

--- src/main/java/com/paulzzh/yuzu/gui/RenderUtils.java
+++ src/main/java/com/paulzzh/yuzu/gui/RenderUtils.java
@@ -52,25 +52,27 @@ public class RenderUtils {
     /**
      * Draws a rectangle with a vertical gradient between the specified colors.
      */
-    protected static void drawGradientRect(int x1, int y1, int x2, int y2, int color, float startAlpha, float endAlpha) {
+    protected static void drawGradientRect(int left, int top, int right, int bottom, int color, float startAlpha, float endAlpha) {
         float f1 = (float) (color >> 16 & 255) / 255.0F;
         float f2 = (float) (color >> 8 & 255) / 255.0F;
         float f3 = (float) (color & 255) / 255.0F;
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         GL11.glEnable(GL11.GL_BLEND);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glDisable(GL11.GL_ALPHA_TEST);
+        OpenGlHelper.glBlendFunc(770, 771, 1, 0);
         GL11.glShadeModel(GL11.GL_SMOOTH);
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
         tessellator.setColorRGBA_F(f1, f2, f3, startAlpha);
-        tessellator.addVertex((double) x1, (double) y2, (double) 0);
-        tessellator.addVertex((double) x1, (double) y1, (double) 0);
+        tessellator.addVertex((double) left, (double) bottom, (double) 0);
+        tessellator.addVertex((double) left, (double) top, (double) 0);
         tessellator.setColorRGBA_F(f1, f2, f3, endAlpha);
-        tessellator.addVertex((double) x2, (double) y1, (double) 0);
-        tessellator.addVertex((double) x2, (double) y2, (double) 0);
+        tessellator.addVertex((double) right, (double) top, (double) 0);
+        tessellator.addVertex((double) right, (double) bottom, (double) 0);
         tessellator.draw();
         GL11.glShadeModel(GL11.GL_FLAT);
         GL11.glDisable(GL11.GL_BLEND);
+        GL11.glEnable(GL11.GL_ALPHA_TEST);
         GL11.glEnable(GL11.GL_TEXTURE_2D);
     }
 }

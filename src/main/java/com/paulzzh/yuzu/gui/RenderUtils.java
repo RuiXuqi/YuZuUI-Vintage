2f703723be2ec59caf28c0d138c40f360a1b6486
paulzzh <28055560+paulzzh@users.noreply.github.com>
2024-12-29 04:15:10 +0800

    启用线性缩放

--- src/main/java/com/paulzzh/yuzu/gui/RenderUtils.java
+++ src/main/java/com/paulzzh/yuzu/gui/RenderUtils.java
@@ -1,9 +1,13 @@
 package com.paulzzh.yuzu.gui;
 
 import net.minecraft.client.renderer.Tessellator;
+import org.lwjgl.opengl.GL11;
 
 public class RenderUtils {
     public static void blit(float x, float y, float width, float height) {
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
         tessellator.addVertexWithUV(x, y + height, 0, 0, 1); // 左下角

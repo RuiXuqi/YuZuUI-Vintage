92238c30b0b75afea67f6219c7c9ab77380159a1
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2025-10-12 16:05:31 +0800

    语音改为立体声，修正en_us，加回线性缩放
    for %f in (*.*) do ffmpeg -i "%f" -ac 2 "%~nf_new%~xf"
    
    我删它干嘛啊（

--- src/main/java/com/paulzzh/yuzu/gui/RenderUtils.java
+++ src/main/java/com/paulzzh/yuzu/gui/RenderUtils.java
@@ -3,12 +3,19 @@ package com.paulzzh.yuzu.gui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 public class RenderUtils {
     public static void blit(float x, float y, float width, float height) {
+        GlStateManager.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+        GlStateManager.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+        GlStateManager.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+        GlStateManager.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferBuilder = tessellator.getBuffer();
         bufferBuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
